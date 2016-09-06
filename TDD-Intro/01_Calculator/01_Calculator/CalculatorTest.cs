3078f77f3a1601b5b38860c451a86917bc13b05f
Kampmarsvin <simon@stochholm.net>
2016-09-06 13:05:04 +0200

    Changed Intro to Functions and added a 2 in Calculator array

--- TDD-Intro/01_Calculator/01_Calculator/CalculatorTest.cs
+++ TDD-Intro/01_Calculator/01_Calculator/CalculatorTest.cs
@@ -22,7 +22,7 @@ namespace _01_Calculator
         public void Sum()
         {
             //Arrange
-            int[] numbers = new int[] { };
+            int[] numbers = new int[2];
             Assert.AreEqual(0, Calculator.Sum(numbers));
 
             numbers[0] = 7;

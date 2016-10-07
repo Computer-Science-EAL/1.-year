3735aa53393dd7851b727f25f74fca32189d3ec1
Kampmarsvin <simon@stochholm.net>
2016-10-07 11:30:41 +0200

    Added Mocking exercise

---
+++ Mocking/Mocking/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mocking
+{
+    public interface IEmployeeRepository
+    {
+        void Clear();
+        int CountEmployees();
+        Employee CreateEmployee(string name, string type);
+        void SaveEmployee(Employee employee);
+        Employee LoadEmployee(int id);
+        List<Employee> FindAllEmployees();
+
+    }
+}
---
+++ Mocking/Mocking/ILoginModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mocking
+{
+    public interface ILoginModule
+    {
+        void Login(User user);
+        void Register(User user);
+        void ResendPassword(User user);
+    }
+}
